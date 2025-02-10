c0968ab04d1b1a8ebc83d37728a6982237491053
George Papidas <george.papidas@gmail.com>
2025-02-10 11:36:29 +0200

    revert to initial of branch

--- src/utils/globalVariables.ts
+++ src/utils/globalVariables.ts
@@ -1,10 +1,5 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL !== "VITE_API_BASE_URL_PLACEHOLDER"
-    ? import.meta.env.VITE_API_BASE_URL
-    : "http://localhost:8000";
-
 const CONFIG = {
-  API_BASE_URL,
+  API_BASE_URL: "https://product-selection-backend.us.aldryn.io",
 };
 
 export default CONFIG;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  define: {
-    "import.meta.env.VITE_API_BASE_URL": JSON.stringify("VITE_API_BASE_URL_PLACEHOLDER"),
-  },
+
+export default defineConfig(() => {
+
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+        define: {
+            "import.meta.env.VITE_API_BASE_URL": JSON.stringify(process.env.VITE_API_BASE_URL || ""),
+        },
+    };
 });
